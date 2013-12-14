fce7ba987f281b7e5383389e832ccda076e7aaa2
Carlos <cgajardo@me.com>
2013-12-14 17:37:51 -0300

    Pre presentación, fin hackaton

--- src/cl/populus/api/dao/BoletinDao.java
+++ src/cl/populus/api/dao/BoletinDao.java
@@ -13,13 +13,18 @@ public class BoletinDao extends BaseDao {
 		Boletin response = null;
 		try {
 			Statement stmt = getConnection().createStatement();
-			PreparedStatement ps = getConnection().prepareStatement("SELECT * FROM populus2.proyecto_de_ley WHERE nro_boletin = ?");
+			PreparedStatement ps = getConnection().prepareStatement("SELECT * FROM proyecto_de_ley WHERE nro_boletin = ?");
 			ps.setString(1, numero);
-			ResultSet rs = stmt.executeQuery(ps.toString());
+			ResultSet rs = ps.executeQuery();
 			if(rs.next()){
 				response = new Boletin();
 				response.setNumero(numero);
-				response.setFechaIngreso(rs.getDate(""));
+				response.setFechaIngreso(rs.getDate("fecha_ingreso"));
+				response.setTitulo(rs.getString("titulo_proyecto"));
+				response.setEtapa(rs.getString("etapa"));
+				response.setUrgencia(rs.getString("urgencia_actual"));
+				response.setLeyNro(rs.getString("nro_ley"));
+				response.setOrigen(rs.getString("camara_origen"));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
